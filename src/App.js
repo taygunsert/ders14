805a22cba55b43c482fc2a181a9d7ec35a35797e
Taygun Sert <taygunsert@outlook.com>
2023-04-16 17:12:48 +0300

    feat: useMemo fonksiyon örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
+import { useMemo, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const hesaplama = useMemo( ()=>{ return uzunsurenHesaplama(count) } , [count] )
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todo, index) => {
+          return <p key={index}>{todo}</p>;
+        })}
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+      <hr />
+      <div>
+        Sayaç: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {hesaplama}
+      </div>
+    </div>
   );
 }
+
+const uzunsurenHesaplama = (num) => {
+  console.log("Calculating...");
+  for (let i = 0; i < 1000000000; i++) {
+    num += 1;
+  }
+  return num;
+};
 
 export default App;
